8fba836f27107195b9095740e485666aa242d308
MaiR42 <mair.escobar@mail.udp.cl>
2026-08-31 20:24:00 -0400

    Corregido ñamñam

--- L1/ping.py
+++ L1/ping.py
@@ -1,24 +1,30 @@
-import sys
+# sudo .venv/bin/python ping.py "larycxpajorj h bnpdarmjm nw anmnb"
 
-if len(sys.argv) > 1: # Debug
+import sys
+from scapy.all import IP, ICMP, Raw, sr1, wrpcap
+import time
+
+if len(sys.argv) > 1:
     arg1 = sys.argv[1]
     print(f"Argumento 1: {arg1}")
 
-palabra_cifrada = arg1 # larycxpajorj h bnpdarmjm nw anmnb
-
-from scapy.all import IP, ICMP, sr1,wrpcap
-import time
-
-packet_dst = "192.168.1.1" # IP destino
+palabra_cifrada = arg1
+packet_dst = "192.168.1.1"
 paquetes_enviados = []
 
-for i,letter in enumerate(palabra_cifrada):
-    Codigo_ASCII = ord(letter)
-    segundos = int(time.time())+i
-    microsegundos = Codigo_ASCII
-    timestamp = segundos + (microsegundos / 1_000_000)
-    packet = IP(dst=packet_dst)/ICMP()
-    packet.time = timestamp
+for i, letter in enumerate(palabra_cifrada):
+    codigo_ascii = ord(letter)
+
+    # Payload estándar de ping Linux: 0x10 a 0x37 (40 bytes)
+    payload = list(range(0x10, 0x38))  # 40 bytes: 0x10..0x37
+
+    # Letra en el primer byte del payload (posición 0x10)
+    payload[0] = codigo_ascii
+
+    payload_bytes = bytes(payload)
+
+    packet = IP(dst=packet_dst) / ICMP(id=0x0001, seq=i) / Raw(load=payload_bytes)
+
     respuesta = sr1(packet, timeout=2)
     paquetes_enviados.append(packet)
 
